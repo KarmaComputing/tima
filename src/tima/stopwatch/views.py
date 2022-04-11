04ea78ba57a69ce4285d89779a6118678c108abf
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-04-11 23:34:38 +0100

    Fix #1 basic signup

--- src/tima/stopwatch/views.py
+++ src/tima/stopwatch/views.py
@@ -1,14 +1,25 @@
-from cmath import log
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+from stopwatch.forms import SignupFirm
 
 def index(request):
     return render(request, "index.html")
 
 
 def sign_up(request):
-    return render(request, "sign-up.html")
+    if request.method == "POST":
+        form = SignupFirm(request.POST)
+        if form.is_valid():
+            breakpoint()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user = User.objects.create_user(username, email, password)
+    else:
+        form = SignupFirm()
+    return render(request, "sign-up.html", {"form": form})
 
 
 @login_required(login_url="/admin")
